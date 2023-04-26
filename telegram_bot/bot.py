ef4e6f1619f9261de09a07989546996d04adf774
Avenir <kadomcev2004@gmail.com>
2023-04-26 23:50:14 +0300

    Changes in bot structure: Added user-bot conversation. Improve readability

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -21,11 +21,16 @@ from telegram.ext import (
     MessageHandler,
     filters,
 )
-sys.path.insert(1, '/Users/avenir/vscode/Python/Tgbot_bpm22-1/telegram_bot/stuff')
-sys.path.insert(2, '/Users/avenir/vscode/Python/Tgbot_bpm22-1/telegram_bot/db')
 
-import telegram_token
-import messages
+from handlers.links_handler import links
+from handlers.start_handler import start
+from buttons.group import group
+from buttons.other_button.birthdays import birthdays
+from buttons.other_button.admin_panel import admin_panel
+from buttons.schedule_button.schedule import schedule, today_tommorow_schedule, choose_weekday, chosen_day
+from buttons.other_button.other import other
+from buttons.other_button.horoscope import horoscope
+from stuff import telegram_token
 
 
 tracemalloc.start()
@@ -37,116 +42,23 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-START_ROUTES, END_ROUTES = range(2)
+MAIN_MENU, SCHEDULE_MENU, CHOOSE_WEEKDAY = range(3)
+OTHER_MENU, USER_MENU, ADMIN_PANEL = range(3, 6)
+ADMIN_HOMEWORK, GROUP_MENU, HOMEWORK_MENU, LINKS_MENU = range(6, 10)
+classes = ['МА', 'ПЭ', 'ДМ', 'ИГ', 'ФЗ']
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Start function to show the main menu."""
-    reply_keyboard = [["/Group", "/Homework", "/Links", "/Schedule", "/Other"]]
+
+
+async def profile(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Function to show user profile"""
+    reply_keyboard = [['Назад']]
     await update.message.reply_text(
-        text=messages.start,
+        text="To be implemented",
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard, one_time_keyboard=True, resize_keyboard=True
         ),
     )
-
-async def stuff(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Function to show useful links."""
-    await update.message.reply_text(
-        text="Ссылки на полезные материалы",
-        reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton(text='Google drive', url=messages.google)],
-            [InlineKeyboardButton(text='Vk group', url=messages.vk)],
-            [InlineKeyboardButton(text='LMS', url=messages.lms)],
-            [InlineKeyboardButton(text='Microsoft teams', url=messages.teams)]
-        ])
-    )
-
-async def other(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Function to show the other functions menu."""
-    await update.message.reply_text("To be implemented")
-
-# /Group Button
-def data(page, limit):
-    """Function to retrieve data about people from the SQLite database for group button."""
-    conn = sqlite3.connect('/Users/avenir/vscode/Python/Tgbot_bpm22-1/telegram_bot/users.db')
-    cmd = conn.cursor()
-    query = f"SELECT * FROM people LIMIT {limit} OFFSET {limit * page}"
-    cmd.execute(query)
-    rows = cmd.fetchall()
-    conn.close()
-
-    keyboard = []
-    for row in rows:
-        id = row[0]
-        keyboard.append([InlineKeyboardButton(text=row[1], callback_data=f'person_{id}')])
-
-    return keyboard
-
-async def group(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Function to show the first page of the group list."""
-    keyboard = data(0, 10)
-    keyboard.append([InlineKeyboardButton("->", callback_data='->2')])
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text("Страница 1", reply_markup=reply_markup)
-    return START_ROUTES
-
-async def start_again(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-    id = query.data.split('_')[1]
-    conn = sqlite3.connect('/Users/avenir/vscode/Python/Tgbot_bpm22-1/telegram_bot/users.db')
-    cmd = conn.cursor()
-    sql_query = f"SELECT * FROM people WHERE people.id = {id}"
-    cmd.execute(sql_query)
-    rows = cmd.fetchall()
-    conn.close()
-    id = rows[0][0]
-    name = rows[0][1]
-    telegram = rows[0][2]
-    # github = rows[0][3]
-    # birhday = rows[0][4]
-    # person_text = f"{name}: \nНомер в группе: {id}\nTelegram: {telegram}\nGithub: {github}\nBirhday: {birhday}"
-    keyboard = []
-    keyboard.append([InlineKeyboardButton("Назад", callback_data='1<-')])
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text(f"{name}: \nTelegram: {telegram}", reply_markup=reply_markup)
-    return START_ROUTES
-
-async def next_page_2(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-    keyboard = data(1, 10)
-    keyboard.append([InlineKeyboardButton("->", callback_data='->3'), InlineKeyboardButton("<-", callback_data='1<-')])
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text("Страница 2", reply_markup=reply_markup)
-    return START_ROUTES
-
-async def next_page_3(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-    keyboard = data(2, 10)
-    keyboard.append([InlineKeyboardButton("<-", callback_data='2<-')])
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text("Страница 3", reply_markup=reply_markup)
-    return START_ROUTES
-
-async def prev_page_1(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-    keyboard = data(0, 10)
-    keyboard.append([InlineKeyboardButton("->", callback_data='->2')])
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text("Страница 1", reply_markup=reply_markup)
-    return START_ROUTES
-
-async def prev_page_2(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-    keyboard = data(1, 10)
-    keyboard.append([InlineKeyboardButton("->", callback_data='->3'), InlineKeyboardButton("<-", callback_data='1<-')])
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await query.edit_message_text("Страница 2", reply_markup=reply_markup)
-    return START_ROUTES
+    return USER_MENU
 
 # /Homework Button
 async def homework(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -158,7 +70,7 @@ async def homework(update: Update, context: ContextTypes.DEFAULT_TYPE):
         keyboard.append([InlineKeyboardButton(button, callback_data=f'class_{id}')])
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text("Выберите предмет:", reply_markup=reply_markup)
-    return START_ROUTES
+    return HOMEWORK_MENU
 
 async def homework_back(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
@@ -171,7 +83,7 @@ async def homework_back(update: Update, context: ContextTypes.DEFAULT_TYPE):
         keyboard.append([InlineKeyboardButton(button, callback_data=f'class_{id}')])
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.callback_query.message.edit_text("Выберите предмет:", reply_markup=reply_markup)
-    return START_ROUTES
+    return HOMEWORK_MENU
 
 def get_homework_text(class_id):
     conn = sqlite3.connect('/Users/avenir/vscode/Python/Tgbot_bpm22-1/telegram_bot/homework.db')
@@ -187,7 +99,7 @@ async def show_homework(update: Update, context: CallbackContext):
     id = query.data.split('_')[1]
     homework_text = get_homework_text(id)
     if homework_text:
-        keyboard = [[InlineKeyboardButton("Назад", callback_data="Назад")]]
+        keyboard = [[InlineKeyboardButton("Назад", callback_data="Назад к списку предметов")]]
         reply_markup = InlineKeyboardMarkup(keyboard)
         await update.callback_query.message.edit_text(homework_text, reply_markup=reply_markup, parse_mode='markdown')
 
@@ -218,115 +130,84 @@ async def set_homework(update: Update, context: CallbackContext):
     else:
         await update.message.reply_text("You are not authorized to use this command.")
 
-# /Schedule Button
-async def schedule(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Function to show the schedule menu."""
-    reply_keyboard = [["Сегодня", "Завтра", "Расписание"]]
-    message = "Пожалуйста, выберите неделю"
+async def start_again(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Start function to show the main menu."""
+    reply_keyboard = [["Группа", "Ссылки", "Другое"], ["Расписание", "Домашка"]]
     await update.message.reply_text(
-        text=message,
-        reply_markup=ReplyKeyboardMarkup(
+        text="Выберите команду",
+        reply_markup= (ReplyKeyboardMarkup(
             reply_keyboard, one_time_keyboard=False, resize_keyboard=True
-        ),
+        )),
     )
-
-    return 0
-
-async def choose_day(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Function to show the timetable for the day."""
-    user_text = update.message.text
-    buttons = ["Сегодня", "Завтра"]
-    week = ["even", "odd"]
-    if user_text not in buttons:
-        await update.message.reply_text(
-            "Пожалуйста, попробуйте снова"
-        )
-    else:
-        day = weekday(update.message.date, buttons.index(user_text))
-        week_desc = week[(update.message.date.isocalendar().week) % 2]
-        message_text = schedule_query(day, week_desc)
-        await update.message.reply_text(
-            message_text
-        )
-
-def weekday(datetime, index):
-    """Function identify user's message weekday."""
-    weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-    day = weekdays[datetime.weekday() + index]
-    return day
-
-def schedule_query(weekday, week):
-    """Function to receive data about classes on particular days of the week."""
-    conn = sqlite3.connect('/Users/avenir/vscode/Python/Tgbot_bpm22-1/telegram_bot/schedule.db')
-    cmd = conn.cursor()
-    query = f"SELECT * FROM classes WHERE week = '{week}' AND day = '{weekday}' ORDER BY position"
-    cmd.execute(query)
-    rows = cmd.fetchall()
-    conn.close()
-
-    text = f"Расписание на {weekday} \n \n"
-    if week == 1:
-        text += "Нижняя неделя \n \n"
-    else:
-        text += "Верхняя неделя: \n \n"
-    for row in rows:
-        text += f"{row[3]} пара ({row[4]} - {row[5]}) \n"
-        text += f"{row[6]} ({row[7]}) \n {row[8]} \n {row[9]} \n \n"
-    return text
-
-async def week(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("To be implemented")
-
+    return MAIN_MENU
 
 # __main__
 if __name__ == '__main__':
     application = ApplicationBuilder().token(telegram_token.TELEGRAM_BOT_TOKEN).build()
-    
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-    stuff_handler = CommandHandler('links', stuff)
-    application.add_handler(stuff_handler)
-    
-    group_conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('group', group)],
-        states={
-            START_ROUTES: [
-                CallbackQueryHandler(next_page_2, pattern="^"+ '->2' + "$"),
-                CallbackQueryHandler(next_page_3, pattern="^"+ '->3' + "$"),
-                CallbackQueryHandler(prev_page_1, pattern="^"+ '1<-' + "$"),
-                CallbackQueryHandler(prev_page_2, pattern="^"+ '2<-' + "$"),
-                CallbackQueryHandler(start_again, pattern="^" + 'person_' + ".+")
-            ],
-        },
-        fallbacks=[CommandHandler("group", group)],
+
+    conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('start', start)],
+    states={
+        MAIN_MENU: [
+            MessageHandler(filters.Regex('^Расписание$'), schedule),
+            MessageHandler(filters.Regex('^Другое$'), other),
+            MessageHandler(filters.Regex('^Группа$'), group.group),
+            MessageHandler(filters.Regex('^Домашка$'), homework),
+            MessageHandler(filters.Regex('^Ссылки$'), links),
+        ],
+        SCHEDULE_MENU: [
+            MessageHandler(filters.Regex('^(Сегодня|Завтра)$'), today_tommorow_schedule),
+            MessageHandler(filters.Regex('^Другие дни$'), choose_weekday),
+            MessageHandler(filters.Regex('^Назад$'), start_again),
+        ],
+        CHOOSE_WEEKDAY: [
+            CallbackQueryHandler(chosen_day, pattern="^" + '.+_.+' "$"),
+            MessageHandler(filters.Regex('^Назад$'), schedule),
+        ],
+        OTHER_MENU: [
+            MessageHandler(filters.Regex('^Назад$'), start_again),
+            MessageHandler(filters.Regex('^Дни рождения$'), birthdays),
+            MessageHandler(filters.Regex('^Гороскоп$'), horoscope.horoscope),
+            MessageHandler(filters.Regex('^Профиль$'), profile),
+            MessageHandler(filters.Regex('^Админ$'), admin_panel.admin),
+        ],
+        USER_MENU: [
+            MessageHandler(filters.Regex('^Назад$'), other),
+        ],
+        ADMIN_PANEL: [
+            MessageHandler(filters.Regex('^Назад$'), other),
+            MessageHandler(filters.Regex('^ДЗ$'), admin_panel.admin_homework),
+            # MessageHandler(filters.Regex('^Уведомления$'), notifications_admin),
+            # MessageHandler(filters.Regex('^Новости$'), news_admin),
+        ],
+        ADMIN_HOMEWORK: [
+            MessageHandler(filters.Regex('^Назад$'), start_again),
+            # MessageHandler(filters.Regex('^(МА|ПЭ|ДМ|ИГ|ФЗ)$'), update_homework),
+        ],
+        GROUP_MENU: [
+            CallbackQueryHandler(group.show_student, pattern="^" + 'person_' + ".+"),
+            CallbackQueryHandler(group.next_page, pattern="^" + '->' + "$"),
+            CallbackQueryHandler(group.prev_page, pattern="^" + '<-' + "$"),
+            MessageHandler(filters.Regex('^Назад$'), start_again),
+        ],
+        HOMEWORK_MENU: [
+            CallbackQueryHandler(show_homework, pattern="^" + 'class_' + ".+"),
+            CallbackQueryHandler(homework_back, pattern="^" + 'Назад к списку предметов' + "$"),
+            MessageHandler(filters.Regex('^Назад$'), start_again)
+        ],
+        LINKS_MENU: [
+            MessageHandler(filters.Regex('^Назад$'), start_again)
+        ],
+    },
+    fallbacks=[MessageHandler(filters.Regex('^Расписание$'), schedule),
+            MessageHandler(filters.Regex('^Другое$'), other),
+            MessageHandler(filters.Regex('^Группа$'), group.group),
+            MessageHandler(filters.Regex('^Домашка$'), homework),
+            MessageHandler(filters.Regex('^Ссылки$'), links),
+            MessageHandler(filters.Regex('/start'), start),
+            CommandHandler('set_homework', set_homework)]
     )
-    application.add_handler(group_conv_handler)
 
-    hw_conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('homework', homework)],
-        states={
-            START_ROUTES: [
-                CallbackQueryHandler(show_homework, pattern="^" + 'class_' + ".+"),
-                CallbackQueryHandler(homework_back, pattern="^" + 'Назад' + "$")
-            ],
-        },
-        fallbacks=[CommandHandler("homework", homework)],
-    )
-    application.add_handler(hw_conv_handler)
-
-    set_hw_handler = CommandHandler('set_homework', set_homework)
-    application.add_handler(set_hw_handler)
-
-    schedule_conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("schedule", schedule)],
-        states={
-            0: [MessageHandler(filters.ALL, choose_day)],
-        },
-        fallbacks=[CommandHandler("schedule", schedule)],
-        )
-    application.add_handler(schedule_conv_handler)
-    
-    other_handler = CommandHandler('other', other)
-    application.add_handler(other_handler)
+    application.add_handler(conv_handler)
     
     application.run_polling()
